1612a32eabe747d3c1e0958164a19a7de2adaa57
Marcos Pontes <114445301+marcos-pontes@users.noreply.github.com>
2023-08-27 11:57:00 -0300

    Update PostsController.ts

--- src/controller/PostsController.ts
+++ src/controller/PostsController.ts
@@ -104,7 +104,13 @@ export class PostsController {
       const output = this.postsBusiness.likeDislikePost(input)
       res.status(201).send(output)
     } catch (error) {
-      
+      if (error instanceof ZodError) {
+        res.status(400).send(error.issues);
+      } else if (error instanceof BaseError) {
+        res.status(error.statusCode).send(error.message);
+      } else {
+        res.status(500).send("Unexpected error");
+      }
     }
   }
 }
